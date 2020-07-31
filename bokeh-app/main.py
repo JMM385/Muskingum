9280fe8dbb97f475929253e9781d4a24f8732c9b
JMM385 <67399246+JMM385@users.noreply.github.com>
2020-07-31 15:38:44 -0300

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -5,6 +5,7 @@
 
 
 # Librerías
+from os.path import join, dirname
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ from bokeh.models import ColumnDataSource, Slider, HoverTool, CDSView, IndexFilt
 # In[2]:
 
 
-df = pd.read_csv('datos_ej2.csv')
+df = pd.read_csv(join(dirname(__file__), 'data/datos_ej2.csv'))
 df.head()
 
 
